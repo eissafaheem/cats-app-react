34662bb8fd2236ffa5e7b0e56fe985e66f975531
Eissa Faheem <eissafaheem@gmail.com>
2023-09-30 10:57:31 +0530

    Feat: user profile ui

--- src/app/home/profile/Profile.component.tsx
+++ src/app/home/profile/Profile.component.tsx
@@ -1,31 +1,108 @@
-import React, {useEffect} from 'react'
-import ProfileStyles from './Profile.module.css'
-import { User } from '../../../client/models/Entities/User'
-import { LocalKeys, LocalStorage } from '../../../client/models/classes/businessLogic/LocalStorage'
-import { AVATARS } from '../../_shared/utils/constatnts'
+import React, { useEffect } from "react";
+import ProfileStyles from "./Profile.module.css";
+import { User } from "../../../client/models/Entities/User";
+import {
+  LocalKeys,
+  LocalStorage,
+} from "../../../client/models/classes/businessLogic/LocalStorage";
+import { AVATARS } from "../../_shared/utils/constatnts";
+import ButtonComponent from "../../_shared/components/button/Button.component";
+import { useProfileHook } from "./Profile.hook";
+import InputComponent from "../../_shared/components/input/Input.component";
 
-type ProfileProps ={
-    user: User
-}
+type ProfileProps = {
+  user: User;
+};
 
 function ProfileComponent(props: ProfileProps) {
+  let { user } = props;
+  const {
+    logoutUser,
+    isEditProfile,
+    setIsEditProfile,
+    name,
+    setName,
+    email,
+    handleProfileButtonClick,
+    password,
+    setEmail,
+    setPassword
+  } = useProfileHook();
 
-    let {user} = props; 
-    
   return (
-    <div className={ProfileStyles['profile-container']}>
-        <div className={ProfileStyles["close-div"]}></div>
-        <div className={ProfileStyles["modal"]}>
-            <header>
-                <img src={AVATARS[user.avatarId]} alt="user avatar" />
-                <div className={ProfileStyles["details"]}>
-                    <h1>{user.name}</h1>
-                    <h3>{user.email}</h3>
+    <div className={ProfileStyles["profile-container"]}>
+      <div className={ProfileStyles["close-div"]}></div>
+      <div className={ProfileStyles["modal"]}>
+        <header>
+          <div className={ProfileStyles["user-info"]}>
+            <img src={AVATARS[user.avatarId]} alt="user avatar" />
+            <div className={ProfileStyles["details"]}>
+              <h1>{user.name}</h1>
+              <h3>{user.pawints} Pawints</h3>
+            </div>
+          </div>
+          <ButtonComponent text="Logout" onClick={logoutUser} />
+        </header>
+        <div className={ProfileStyles["content"]}>
+          <div className={ProfileStyles["avatars"]}>
+            {AVATARS.map((avatar: string, index: number) => {
+              return (
+                <div
+                  className={`${ProfileStyles["avatar"]} ${user.pawints < (index + 1) * 10 &&
+                    ProfileStyles["locked-avatar"]
+                    }`}
+                >
+                  <img src={avatar} alt="user avatar" />
+                  {(index + 1) * 10}
                 </div>
-            </header>
+              );
+            })}
+          </div>
+          <div className={ProfileStyles["user-info"]}>
+            <div className="user-name">
+              <span>Name </span>
+              {
+                isEditProfile
+                  ?
+                  <InputComponent placeholder="Name" setValue={setName} />
+                  :
+                  <>
+                    {user.name}
+                  </>
+              }
+            </div>
+            <div className="user-email">
+              <span>Email </span>
+              {
+                isEditProfile
+                  ?
+                  <InputComponent placeholder="Email" setValue={setEmail} />
+                  :
+                  <>
+                    {user.email}
+                  </>
+              }
+            </div>
+            <div className="user-password">
+              <span>Password </span>
+              {
+                isEditProfile
+                  ?
+                  <InputComponent placeholder="Password" setValue={setPassword} />
+                  :
+                  <>
+                    ********
+                  </>
+              }
+            </div>
+          </div>
         </div>
+        <footer>
+          <ButtonComponent text={isEditProfile?"Save Profile":"Edit Profile"} onClick={handleProfileButtonClick} />
+        </footer>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProfileComponent
+export default ProfileComponent;
--- src/app/home/profile/Profile.hook.ts
+++ src/app/home/profile/Profile.hook.ts
@@ -1,5 +1,40 @@
-export const useProfileHook = () =>{
-    return {
+import { useState } from "react";
 
+export const useProfileHook = () =>{
+
+    const [isEditProfile, setIsEditProfile] = useState<boolean>(false);
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    function logoutUser(){
+
+    }
+
+    function handleProfileButtonClick(){
+        if(isEditProfile){
+            updateProfile()
+        }
+        else{
+            setIsEditProfile(true);
+        }
+    }
+    
+    function updateProfile(){
+        setIsEditProfile(false);
+
+    }
+
+    return {
+        logoutUser,
+        isEditProfile,
+        setIsEditProfile,
+        name,
+        setName,
+        email,
+        setEmail,
+        password,
+        setPassword,
+        handleProfileButtonClick
     };
 }
