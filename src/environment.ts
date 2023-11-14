b80a37d77dd2d7fc2faaaca2ba4513fd44bc9821
Eissa Faheem <eissafaheem@gmail.com>
2023-11-14 20:42:37 +0530

    Feat: added dev environment urls

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-    BASE_URL: "https://cats-app-backend.onrender.com/api",
-    IO_URL: "https://cats-app-backend.onrender.com"
+    // BASE_URL: "https://cats-app-backend.onrender.com/api",
+    // IO_URL: "https://cats-app-backend.onrender.com"
+    BASE_URL: "http://localhost:5000/api",
+    IO_URL: "http://localhost:5000/"
 }
